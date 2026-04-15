cff9c775086bfd95a69d43d6c0bcf27b8001e262
jishan <jk101793@gmail.com>
2026-04-15 05:48:07 +0530

    added opps_Revision01j.java

---
+++ opps_Revision01j.java
@@ -0,0 +1,39 @@
+
+/* //  lass and object topics
+class student{
+    String name;
+    int age;
+    String gender;
+
+void display(){
+    System.out.println( " student name " + name);
+    System.out.println( " student age " + age);
+    System.out.println( " Gender " + gender);
+};
+
+}*/
+
+
+public class opps_Revision01j {
+public static void main(String[] args) {
+
+
+
+
+
+
+    
+   /*  
+student s1 = new student();
+
+s1.name = "jishan khan";
+s1.age = 25;
+s1.gender = "male";
+
+s1.display();*/
+
+
+
+
+} 
+}
